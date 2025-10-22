39d86cd7336e0af0af9e1e47da19b580d15ecf0d
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2025-10-22 16:54:16 -0230

    max width on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,7 +111,7 @@ export default function RootLayout({
             <CloudsBackground />
             <div className="relative flex min-h-screen flex-col">
               <Header />
-              <main className="mx-auto max-w-3xl flex-1 py-16 lg:px-8">
+              <main className="mx-auto max-w-5xl flex-1 py-16 lg:px-8">
                 {children}
               </main>
               <Footer />
