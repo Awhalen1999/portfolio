398f4a26910c793ff26cf5ed61903662e6d406ce
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2025-10-23 23:11:43 -0230

    root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
             <CloudsBackground />
             <div className="relative flex min-h-screen flex-col">
               <Header />
-              <main className="mx-auto max-w-2xl flex-1 py-16 lg:px-8">
+              <main className="mx-auto w-full max-w-2xl flex-1 px-4 py-16">
                 {children}
               </main>
               <Footer />
